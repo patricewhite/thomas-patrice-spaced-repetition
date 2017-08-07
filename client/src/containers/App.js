a4c04c80623f666450ca26fd5ea6c1cad4c8cbf7
tckastanek <tckastanek@gmail.com>
2017-08-07 16:05:35 -0400

    Cleaned up comments.

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -7,43 +7,13 @@ import QuestionPage from '../views/QuestionPage';
 import LoginPage from '../views/LoginPage';
 
 export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   currentUser: null
-    // };
-  }
 
   componentDidMount() {
-        // Job 4: Redux-ify all of the state and fetch calls to async actions.
     const accessToken = Cookies.get('accessToken');
 
     if (accessToken) {
       this.props.dispatch(actions.fetchUser(accessToken));
     }
-
-    // if (accessToken) {
-    //   fetch('/api/me', {
-    //     headers: {
-    //       'Authorization': `Bearer ${accessToken}`
-    //     }
-    //   }).then(res => {
-    //     if (!res.ok) {
-    //       if (res.status === 401) {
-    //                     // Unauthorized, clear the cookie and go to
-    //                     // the login page
-    //         Cookies.remove('accessToken');
-    //         return;
-    //       }
-    //       throw new Error(res.statusText);
-    //     }
-    //     return res.json();
-    //   }).then(currentUser =>
-    //             this.setState({
-    //               currentUser
-    //             })
-    //         );
-    // }
   }
 
   render() {
