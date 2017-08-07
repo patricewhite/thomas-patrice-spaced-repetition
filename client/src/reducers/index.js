951a29737362c7bf6ef569b0cc535e06db9af440
tckastanek <tckastanek@gmail.com>
2017-08-07 15:06:05 -0400

    Stubbed out reducer.

---
+++ client/src/reducers/index.js
@@ -0,0 +1,15 @@
+import * as actions from '../actions';
+
+const initialState = {
+  loading: false,
+  error: null,
+  questions: [],
+  currentUser: null,
+  accessToken: null,
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
