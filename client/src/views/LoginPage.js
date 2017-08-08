bf0ae23228caf6b0b4f732dac49dad37f7048ce3
tckastanek <tckastanek@gmail.com>
2017-08-08 11:29:20 -0400

    Componentized the content boxes.

--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import ContentBox from '../components/login/ContentBox';
 
 export default function LoginPage() {
+  const loginContent = {
+    box1: {
+      imgSrc: 'http://via.placeholder.com/250x250',
+      imgAlt: 'Categories',
+      title: 'Select Vim command categories to practice.'
+    },
+    box2: {
+      imgSrc: 'http://via.placeholder.com/250x250',
+      imgAlt: 'Sessions',
+      title: 'Save your sessions...'
+    },
+    box3: {
+      imgSrc: 'http://via.placeholder.com/250x250',
+      imgAlt: 'Other Stuff',
+      title: 'Here is some other stuff...'
+    }
+  };
   return (
     <div>
       <img src='http://via.placeholder.com/1300x500' alt='Hero' />
       <h1>How Do I Vim?</h1>
       <p>Learn how to more than just exit.</p>
       <div className='login-content-container'>
-        <div className='login-content-box'>
-          <img src='http://via.placeholder.com/250x250' alt='Categories' />
-          <h3>Select Vim command categories to practice.</h3>
-        </div>
-        <div className='login-content-box'>
-          <img src='http://via.placeholder.com/250x250' alt='Sessions' />
-          <h3>Save your sessions...</h3>
-        </div>
-        <div className='login-content-box'>
-          <img src='http://via.placeholder.com/250x250' alt='Other Stuff' />
-          <h3>Here is some other stuff...</h3>
-        </div>
+        <ContentBox
+          imgSrc={loginContent.box1.imgSrc}
+          imgAlt={loginContent.box1.imgAlt}
+          title={loginContent.box1.title}
+        />
+        <ContentBox
+          imgSrc={loginContent.box2.imgSrc}
+          imgAlt={loginContent.box2.imgAlt}
+          title={loginContent.box2.title}
+        />
+        <ContentBox
+          imgSrc={loginContent.box3.imgSrc}
+          imgAlt={loginContent.box3.imgAlt}
+          title={loginContent.box3.title}
+        />
       </div>
       <div className='login-auth-container'>
         <h3>Interested? Click below to login or signup!</h3>
