34b9fe2c7456120ff7b608634fc298680561d90e
tckastanek <tckastanek@gmail.com>
2017-08-08 10:15:22 -0400

    Inital commit to loginscreen branch.

--- client/src/views/LoginPage.js
+++ client/src/views/LoginPage.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function LoginPage() {
-  return <a href={'/api/auth/google'}>Login with Google</a>;
+  return (
+    <div>
+      <img src='http://via.placeholder.com/500x200' />
+      <h1>How Do I Vim?</h1>
+      <p>Learn how to more than just exit.</p>
+      <a href={'/api/auth/google'}>Login with Google</a>
+    </div>
+  );
 }
