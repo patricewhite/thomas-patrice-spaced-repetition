c7aad5e129ea53cf5964626c7f67f0a14acc2898
tckastanek <tckastanek@gmail.com>
2017-08-09 15:44:50 -0400

    Refactored props needed for scoreboard for consistency.

--- client/src/components/nav/NavBar.js
+++ client/src/components/nav/NavBar.js
@@ -3,7 +3,7 @@ import '../../styles/NavBar.css';
 
 
 export default function NavBar(props) {
-  const { profileImg, currentStreak, questionsCorrect, totalQuestions } = props;
+  const { profileImg, currentStreak, totalCorrect, totalAnswered } = props;
   return (
     <div className='nav-bar-container'>
       <div className='profile-image'>
@@ -15,7 +15,7 @@ export default function NavBar(props) {
 
       <div className='scoreboard-container'>
         <h3>{`Your current streak is ${currentStreak}`}</h3>
-        <h3>{`Total Score: ${questionsCorrect}/${totalQuestions}`}</h3>
+        <h3>{`Total Score: ${totalCorrect}/${totalAnswered}`}</h3>
       </div>
       <div className='account-controls'>
         <a
